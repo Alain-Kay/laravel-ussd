7e8d19c621dbc1d2d17aa65ec1c5a1d46a757fb0
Alain Kay <kazadikayumba@gmail.com>
2025-09-18 12:48:51 +0200

    adding student model and migration

---
+++ app/Models/Student.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'first_name',
+        'middle_name',
+        'last_name',
+        'registration_number',
+        'gender',
+    ];
+}
