dd5a824dd1b266372f4b5a5ec71e1c108dc35d8c
Alain Kay <kazadikayumba@gmail.com>
2025-09-18 12:39:24 +0200

    adding shool model and migration

---
+++ app/Models/School.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class School extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'name',
+        'code',
+        'director',
+        'address',
+        'phone',
+    ];
+
+}
