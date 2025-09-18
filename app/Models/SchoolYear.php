d3f32c1637a6da58843b2cea8324308b9596074a
Alain Kay <kazadikayumba@gmail.com>
2025-09-18 12:44:28 +0200

    adding shool year model and migration

---
+++ app/Models/SchoolYear.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SchoolYear extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'year',
+        'start_date',
+        'end_date',
+        'is_active',
+    ];
+
+}
